bdd50dbf2c92b85541d1df31d7b37a38e54b1dfe
HalFsiDe69 <120192579+HalFsiDe69@users.noreply.github.com>
2023-01-10 12:04:20 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,9 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    turtle.turnLeft()
+})
+input.onButtonPressed(Button.B, function () {
+    turtle.turnRight()
+})
+input.onLogoEvent(TouchButtonEvent.Pressed, function () {
+    turtle.forward(1)
 })
